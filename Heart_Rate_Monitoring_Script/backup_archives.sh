7a5f5a959fd8a68128ac0c3eea3ad693ebb00b42
Rwigenzadavy <r.niyoyande@alustudent.com>
2024-10-20 10:38:07 +0200

    Update backup_archives.sh

--- Heart_Rate_Monitoring_Script/backup_archives.sh
+++ Heart_Rate_Monitoring_Script/backup_archives.sh
@@ -1,4 +1,23 @@
 #!/usr/bin/env bash
-tar -czf backup_$(date +"%Y%m%d").tar.gz archive/
-echo "Backup created."
 
+# Variables
+group_number="group12" 
+archive_dir="archived_logs_group$group_number"
+username="fdb056dd42a9" 
+host="fdb056dd42a9.6b7419f6.alu-cod.online" 
+remote_dir="/home/"
+
+# Create the archive directory if it doesn't exist
+mkdir -p "$archive_dir"
+
+# Move archived log files to the new directory
+mv heart_rate_log.txt_* "$archive_dir/"
+echo "Moved archived log files to: $archive_dir"
+
+# Backup the contents to the remote server
+scp -r "$archive_dir/" "$username@$host:$remote_dir"
+if [ $? -eq 0 ]; then
+    echo "Backup successful."
+else
+    echo "Backup failed."
+fi
