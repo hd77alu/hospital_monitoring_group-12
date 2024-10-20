974b3afb059c09975165a4d95deb72277b27594f
Rwigenzadavy <r.niyoyande@alustudent.com>
2024-10-20 10:39:09 +0200

    archive log

--- Heart_Rate_Monitoring_Script/archive_log.sh
+++ Heart_Rate_Monitoring_Script/archive_log.sh
@@ -1,5 +1,18 @@
 #!/usr/bin/env bash
-mkdir -p archive
-mv heart_rate_log.txt archive/heart_rate_log_$(date +"%Y%m%d_%H%M%S").txt
-echo "Log archived."
 
+# Original log file
+log_file="heart_rate_log.txt"
+
+# Check if the log file exists
+if [ ! -f "$log_file" ]; then
+    echo "Log file does not exist."
+    exit 1
+fi
+
+# Get the current timestamp
+timestamp=$(date +"%Y%m%d_%H%M%S")
+new_log_file="${log_file}_${timestamp}"
+
+# Archive the log file
+mv "$log_file" "$new_log_file"
+echo "Archived log file as: $new_log_file"
